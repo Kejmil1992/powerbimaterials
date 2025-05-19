dc6dddf869b7fc61e3ac683ec0df2401f421fcb8
Kejmil1992 <66531482+Kejmil1992@users.noreply.github.com>
2025-05-19 20:48:48 +0200

    Update Sharepoint file name parameter.m

--- PowerQuery/Sharepoint file name parameter.m
+++ PowerQuery/Sharepoint file name parameter.m
@@ -1,1 +1,1 @@
-"KPI_Wind_2023 PBI Mockup.xlsx" meta [IsParameterQuery = true, IsParameterQueryRequired = true, Type = type text]
+"Excel file.xlsx" meta [IsParameterQuery = true, IsParameterQueryRequired = true, Type = type text]
